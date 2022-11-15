545c5da114c54692761c0f70dca27a692e00ccd2
Nafri <irfanfmurad@gmail.com>
2022-11-15 13:39:12 +0800

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xcdd3ffa5a8413a194d3e99eaebc33e4d07e0c3da";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
